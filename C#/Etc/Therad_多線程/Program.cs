85853c94203e205b905ff80895c704d03d2b9429
aandy300 <aandy300@gmail.com>
2021-05-16 17:38:53 +0800

    2021.05.16 updata C#多線程

---
+++ C#/Etc/Therad_多線程/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Threading;
+
+namespace MultithreadingApplication
+{
+    class ThreadCreationProgram
+    {
+        public static void CallToChildThread()
+        {
+            try
+            {
+
+                Console.WriteLine("Child thread starts");
+                // 计数到 10
+                for (int counter = 0; counter <= 10; counter++)
+                {
+                    Thread.Sleep(500);
+                    Console.WriteLine(counter);
+                }
+                Console.WriteLine("Child Thread Completed");
+
+            }
+            catch (ThreadAbortException e)
+            {
+                Console.WriteLine("Thread Abort Exception:" + e);
+            }
+            finally
+            {
+                Console.WriteLine("Couldn't catch the Thread Exception");
+            }
+
+        }
+      
+        static void Main(string[] args)
+        {
+            ThreadStart childref = new ThreadStart(CallToChildThread);
+            // childref(); // 直接執行 不用 .Start()
+            Console.WriteLine("In Main: Creating the Child thread");
+            Thread childThread = new Thread(childref);
+            childThread.Start();
+            // 停止主线程一段时间
+            Thread.Sleep(2000);
+            // 现在中止子线程
+            Console.WriteLine("In Main: Aborting the Child thread");
+            // childThread.Abort(); // 舊的已停用 改用 Interrupt()
+            childThread.Interrupt();
+            
+            Console.ReadKey();
+        }
+    }
+}
---
+++ C#/Etc/Therad_多線程/多線程_可帶參數Ver.cs
@@ -0,0 +1,32 @@
+﻿// using System;
+// using System.Threading;
+
+// namespace MultithreadingApplication
+// {
+//     class ThreadCreationProgram
+//     {
+//          static void Main(string[] args)
+//         {
+//             Thread t1 = new Thread(new ThreadStart(TestMethod));
+//             Thread t2 = new Thread(new ParameterizedThreadStart(TestMethod));
+//             // 前景背景執行
+//             t1.IsBackground = true;
+//             t2.IsBackground = true;
+//             t1.Start();
+//             t2.Start("hello");
+//             Console.ReadKey();
+//         }
+
+//         public static void TestMethod()
+//         {
+//             Console.WriteLine("不帶參數的線程函式");
+//         }
+
+//         public static void TestMethod(object data)
+//         {
+//             // string datastr = data as string;
+//             Console.WriteLine("帶參數的線程函式，參數為：{0}", data);
+//         }
+//     }
+// }
+
