6ff0ff697528fa29cfac91e0097bfd8462f125f1
aandy300 <aandy300@gmail.com>
2022-02-15 22:22:00 +0800

    up w4、基本指令

---
+++ Vue/w4/version_01/card2.js
@@ -0,0 +1,8 @@
+export default {    
+        data() {
+          return {
+            text: '我是區域註冊！'
+          }
+        },
+        template: '<div> 區域註冊: {{ text }} </div>'      
+};
---
+++ Vue/w4/version_01/component.js
@@ -0,0 +1,7 @@
+export default {
+  template: `<div class="card" style="width: 18rem;">
+    <div class="card-body">
+      <h5 class="card-title">import Component</h5>
+    </div>
+  </div>`,
+};
